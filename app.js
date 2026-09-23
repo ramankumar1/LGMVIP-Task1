1697a9a6b738931b6d6ec27778151defe09f85c1
Raman Kumar <88982591+ramankumar1@users.noreply.github.com>
2026-09-24 01:36:33 +0530

    Update app.js
    i change line 22 active to decative

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ document.addEventListener('scroll', () => {
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
+		hamburger.classList.toggle('Decative');
 		mobile_menu.classList.toggle('active');
 	});
 });
